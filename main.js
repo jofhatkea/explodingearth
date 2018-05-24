2380a2367fd7edc6a59d11fab83e4a02b946929e
Jonas Holbech <jh@jonasholbech.dk>
2018-05-24 13:16:18 +0200

    sorta working, sort of broken

--- main.js
+++ main.js
@@ -15,5 +15,16 @@
  loadSVG("assets/sarah.svg", "div", function(){
      console.log("sarah loaded");
      document.querySelector("#booms").style.display="none";
+     let stuff = document.querySelectorAll(".land path, .water path")
+         console.log(stuff)
+         document.querySelector("svg").addEventListener("click", ()=>{
+            stuff.forEach((el,i)=>{
+                console.log(i)
+                setTimeout(()=>{
+                   el.style.transform = `translate(${Math.random()*100-50}px, ${Math.random()*100-50}px)`;
+                }, i*10)
+                
+            })
+         })
      
  });
